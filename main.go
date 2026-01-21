3f33fe0801dfe812c90d370844f878b5b6aa9687
Aldo Camacho <me@aldocamacho.com>
2026-01-20 17:05:22 -0800

    copy files

--- main.go
+++ main.go
@@ -95,6 +95,34 @@ ORDER BY id;
 	}
 }
 
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	srcInfo, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+	if !srcInfo.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
+
+	dstFile, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0644)
+	if err != nil {
+		if errors.Is(err, os.ErrExist) {
+			return fmt.Errorf("File '%s' already exists.", dst)
+		}
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	return err
+}
+
 func replaceExtension(path, ext string) string {
 	idx := strings.LastIndex(path, ".")
 	if idx == -1 {
